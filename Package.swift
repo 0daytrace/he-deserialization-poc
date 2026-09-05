11b54af1720f2d23e928bdbf1ec9c27f0257fb3c
Salman <0daytrace@gmail.com>
2026-09-05 07:31:28 +0000

    Update macOS platform version to 13.0

--- Package.swift
+++ Package.swift
@@ -1,50 +1,18 @@
 // swift-tools-version: 5.9
-
 import PackageDescription
 
-
-
 let package = Package(
-  
     name: "HE_PoC",
-  
-    platforms: [.macOS(.v13)],
-  
+    platforms: [.macOS(.v26)],
     dependencies: [
-      
         .package(url: "https://github.com/apple/swift-homomorphic-encryption", branch: "main")
-      
     ],
-  
     targets: [
-      
         .executableTarget(
-          
             name: "HE_PoC",
-          
             dependencies: [
-              
                 .product(name: "HomomorphicEncryption", package: "swift-homomorphic-encryption")
-              
             ]
-          
         )
-      
     ]
-  
 )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
